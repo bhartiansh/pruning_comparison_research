9cb39b993b58d3533d42b59308ed458dd076d798
bhartiansh <139680027+bhartiansh@users.noreply.github.com>
2025-04-13 16:00:23 +0530

    Update tinyimagenet_preprocessing.py

--- tinyimagenet_preprocessing.py
+++ tinyimagenet_preprocessing.py
@@ -1,7 +1,7 @@
 import os
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
-def load_tiny_imagenet_data(data_dir='./tiny-imagenet-200', target_size=(224, 224), batch_size=64):
+def load_tiny_imagenet_data(data_dir='./tiny-imagenet-200/tiny-imagenet-200', target_size=(224, 224), batch_size=64):
     train_dir = os.path.join(data_dir, 'train')
     val_dir = os.path.join(data_dir, 'val')
 
