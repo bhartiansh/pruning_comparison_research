cafd882c91b294001462095ecc827770233a5d43
bhartiansh <139680027+bhartiansh@users.noreply.github.com>
2025-04-13 15:57:49 +0530

    Add files via upload

--- tinyimagenet_preprocessing.py
+++ tinyimagenet_preprocessing.py
@@ -1,25 +1,24 @@
+import os
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
-def load_tiny_imagenet_data(data_dir='./tiny-imagenet-200/tiny-imagenet-200', target_size=(224, 224), batch_size=64):
-    datagen = ImageDataGenerator(
-        rescale=1./255,
-        validation_split=0.2
-    )
+def load_tiny_imagenet_data(data_dir='./tiny-imagenet-200', target_size=(224, 224), batch_size=64):
+    train_dir = os.path.join(data_dir, 'train')
+    val_dir = os.path.join(data_dir, 'val')
+
+    datagen = ImageDataGenerator(rescale=1./255)
 
     train_gen = datagen.flow_from_directory(
-        directory=f"{data_dir}/train",
+        train_dir,
         target_size=target_size,
         batch_size=batch_size,
-        class_mode='categorical',
-        subset='training'
+        class_mode='categorical'
     )
 
     val_gen = datagen.flow_from_directory(
-        directory=f"{data_dir}/train",
+        val_dir,
         target_size=target_size,
         batch_size=batch_size,
-        class_mode='categorical',
-        subset='validation'
+        class_mode='categorical'
     )
 
     return train_gen, val_gen
